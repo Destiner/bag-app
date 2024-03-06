71976fa29c005bcd9e65a9c0e84434feb3b2e1f7
Timur Badretdinov <accounts@destiner.io>
2024-03-06 05:44:59 +0000

    Fix imports

--- server/api/frame/claim-token/start.ts
+++ server/api/frame/claim-token/start.ts
@@ -1,14 +1,12 @@
 import { getFrameMessage, getFrameHtmlResponse } from "@coinbase/onchainkit";
 import type { FrameRequest } from "@coinbase/onchainkit";
-import { parseEther type Hex, Address, formatEther } from "viem";
+import { type Hex, Address, formatEther } from "viem";
 
 import { getImageUrl } from "~/utils/frames/claim-token";
 import {
   getWalletAddress,
   getErrorImageUrl,
   getTokenBalance,
-  multiExecute,
-  getErc20TransferData,
 } from "~/utils/frames";
 
 const config = useRuntimeConfig();
@@ -16,11 +14,8 @@ const config = useRuntimeConfig();
 const baseUrl = config.public.baseUrl as string;
 const neynarApiKey = config.neynarApiKey as string;
 const privateKey = config.aaPrivateKey as Hex;
-const pimlicoApiKey = config.pimlicoApiKey as string;
 
-const adminAddress = "0xa75a19Cae746f1058d3217Cb6367effD93c73B53";
 const tokenAddress = "0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed";
-const FEE_DENOMINATOR = BigInt(5); // 20%
 
 export default defineEventHandler(async (event) => {
   const body = await readBody<FrameRequest>(event);
@@ -68,9 +63,14 @@ export default defineEventHandler(async (event) => {
     });
   }
 
-  const balanceString = formatEther(tokenBalance)
+  const balanceString = formatEther(tokenBalance);
   return getFrameHtmlResponse({
-    image: getImageUrl(baseUrl, `${balanceString} $DOG`, "your balance", '20% fee is applied to cover gas fees and dev costs'),
+    image: getImageUrl(
+      baseUrl,
+      `${balanceString} $DOG`,
+      "your balance",
+      "20% fee is applied to cover gas fees and dev costs"
+    ),
     buttons: [
       {
         label: "Claim",
